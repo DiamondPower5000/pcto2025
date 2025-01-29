8c6a22bd0fcd943a11948236d81254b64960ebe1
Federico Pascucci <federico.pascucci2@gmail.com>
2025-01-29 16:34:10 +0100

    Deleted all repository contents

--- IssPositionAPI/Models/IssPositionRecord.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class IssPositionRecord
-{
-    public int Id { get; set; }
-    public string Latitude { get; set; }
-    public string Longitude { get; set; }
-    public DateTime Timestamp { get; set; }
-}
