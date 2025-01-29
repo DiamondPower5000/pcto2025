4279ef1fa08aaeba63e30e12aaa8fdf5d7f98efd
Federico Pascucci <federico.pascucci2@gmail.com>
2025-01-29 16:28:02 +0100

    Tolti file posizione iss e applicazione flutter

---
+++ IssPositionAPI/Models/IssPositionRecord.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class IssPositionRecord
+{
+    public int Id { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    public DateTime Timestamp { get; set; }
+}
